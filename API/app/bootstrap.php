9169c6129cbae42b405dd0a71de2045455a3305b
TerryFunggg <terryyessfung@gmail.com>
2021-02-22 20:52:17 +0800

    update Request

--- API/app/bootstrap.php
+++ API/app/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 require __DIR__ . '/core/DB.php';
 require __DIR__ . '/core/Router.php';
+require __DIR__ . '/core/Request.php';
 
 //$db = new DB;
 
 $router = new Router;
 $router->setRoutes($routes);
 
-$url = $_SERVER['REQUEST_URI'];
-require __DIR__ . '/api/' . $router($url);
+Request::recived();
--- API/app/core/Request.php
+++ API/app/core/Request.php
@@ -1,5 +1,36 @@
 <?php
 
 class Request {
+    private $api = '404';
+    private static $request = null;
+
+    private function __construct()
+    {
+        $uri = $this->getUri();
+        if(file_exists(PROJECT_ROOT . 'app/api/' . $uri[1] . '.php')){
+            $this->api = $uri[1];
+        }
+
+        // if the api file dones't exited, it will redirct to 404 statuc
+        require_once PROJECT_ROOT . 'app/api/' . $this->api . '.php';
+
+    }
+
+    private function getUri ()
+    {
+        if(isset($_SERVER['REQUEST_URI']))
+        {
+            $uri = rtrim($_SERVER['REQUEST_URI'], '/');
+            $uri = filter_var($uri, FILTER_SANITIZE_URL);
+            $uri = explode('/', $uri);
+            return $uri;
+        }
+    }
+
+    public static function recived()
+    {
+        static::$request ?? static::$request = new Request();
+    }
+
 
 }

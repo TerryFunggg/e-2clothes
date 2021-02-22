933c731e3709df0ff9668dd4f4dbff985e214a14
TerryFunggg <terryyessfung@gmail.com>
2021-02-22 16:40:48 +0800

    update basic files

---
+++ API/config/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+$routes = [
+    'users' => 'posts.php',
+];
---
+++ API/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require "./vendor/autoload.php";
+require __DIR__ . "/config/setting.php";
+require __DIR__ . "/app/bootstrap.php";

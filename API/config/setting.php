0d98bd958448724bd1a516e241f94bee48389e56
TerryFunggg <terryyessfung@gmail.com>
2021-02-22 18:51:17 +0800

    update config file

--- API/config/setting.php
+++ API/config/setting.php
@@ -1,3 +1,13 @@
 <?php
 
 define("PROJECT_ROOT", __DIR__ . '/../');
+define("PROJECT_NAME", "e2clothes");
+
+$_ENV['environment'] = $_ENV['environment'] ?? 'development';
+
+// Error config
+ini_set('display_errors', $_ENV['environment'] == 'development');
+ini_set('error_log', PROJECT_ROOT .  'phperror.txt');
+error_reporting(E_ALL);
+
+date_default_timezone_set('Asia/Shanghai');

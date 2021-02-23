f8fcfa13444e7ded3ea417943abebc29938cc66b
TerryFunggg <terryyessfung@gmail.com>
2021-02-23 16:57:11 +0800

    update user validation

--- e2ClothesAPI/app/models/user.rb
+++ e2ClothesAPI/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :email,      presence: true
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name,  presence: true, length: { minimum: 2 }
+  validates :email,      presence: true, uniqueness: true
+  validates :password,   presence: true, length: { in: 6..20 }
 end

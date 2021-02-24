3e8a0d57cb25b3317f0a0f2653773ee7caef7f5f
TerryFunggg <terryyessfung@gmail.com>
2021-02-24 20:41:12 +0800

    update create address mutation

---
+++ e_clothes_api/app/graphql/mutations/create_address.rb
@@ -0,0 +1,35 @@
+module Mutations
+class CreateAddress < BaseMutation
+  argument :street_address, String, required: true
+  argument :secondary_address, String, required: true
+  argument :building_address, String, required: true
+  argument :zip_code, String, required: true
+  argument :city, String, required: true
+
+  field :address, Types::AddressType, null: false
+  field :errors, [String], null:false
+
+  def resolve(city:, street_address:, secondary_address:, building_address:, zip_code:)
+    City.create(name: city) if City.where(name: city).empty?
+    city = City.find_by(name: city)
+    address = Address.new(
+        street_address:     street_address,
+        secondary_address:  secondary_address,
+        building_address:   building_address,
+        zip_code:           zip_code,
+        city_id:            city.id)
+
+    if address.save
+      {
+        address: address,
+        errors: []
+      }
+    else
+      {
+        user: nil,
+        errors: address.errors.full_messages
+      }
+    end
+  end
+end
+end

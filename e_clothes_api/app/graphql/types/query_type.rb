846504663ca84d2c5af99c5f7afa1925f66b8760
TerryFunggg <terryyessfung@gmail.com>
2021-02-24 21:05:17 +0800

    updae user query

--- e_clothes_api/app/graphql/types/query_type.rb
+++ e_clothes_api/app/graphql/types/query_type.rb
@@ -7,16 +7,33 @@ module Types
     # /address
     field :addresses, [Types::AddressType], null: false
 
-    def addresses
-      Address.all
-    end
-
     field :address, Types::AddressType, null: false do
       argument :id, ID, required: true
     end
 
+    # /user
+    field :users, [Types::UserType], null: false
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+
+
+    def addresses
+      Address.all
+    end
+
     def address(id:)
       Address.find(id)
+    end
+
+    def users
+      User.all
+    end
+
+    def user(id:)
+      User.find(id)
     end
   end
 end
---
+++ e_clothes_api/app/graphql/types/user_type.rb
@@ -0,0 +1,22 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :email, String, null: false
+    field :password, String, null: false
+    field :avator, String, null: true
+    field :phone, String, null: false
+    field :is_active, Boolean, null: true
+    field :last_login, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :full_name, String, null: true
+
+
+    def full_name
+        "#{object.first_name} #{object.last_name}"
+    end
+  end
+end

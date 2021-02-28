4cd85efbfb9589ec66f46cb1b7aa75f3bc8fdcd1
TerryFunggg <terryyessfung@gmail.com>
2021-02-28 15:53:24 +0800

    update seed

--- e_clothes_api/db/seeds.rb
+++ e_clothes_api/db/seeds.rb
@@ -5,24 +5,57 @@
     })
 end
 
+20.times do |index|
 
-10.times do |index|
-  Address.create(
-    city_id: rand(1...20),
-    street_address: Faker::Address.street_address,
-    secondary_address: Faker::Address.secondary_address,
-    building_address: Faker::Address.building_number,
-    zip_code: Faker::Address.zip_code
+    address = Address.new(
+      city_id: rand(1...20),
+      street_address: Faker::Address.street_address,
+      secondary_address: Faker::Address.secondary_address,
+      building_address: Faker::Address.building_number,
+      zip_code: Faker::Address.zip_code
   )
-
-  User.create(
-    {
-      address_id:   index + 1,
+    user = User.new(
       first_name:   Faker::Name.first_name ,
       last_name:    Faker::Name.last_name,
       email:        Faker::Internet.email,
       password:     Faker::Internet.password(min_length: 6, max_length: 20),
       avator:       Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
       phone:        Faker::PhoneNumber.cell_phone
-    })
+    )
+  user.address = address
+  user.save
+end
+
+5.times do |index|
+  address = Address.new(
+      city_id: rand(1...20),
+      street_address: Faker::Address.street_address,
+      secondary_address: Faker::Address.secondary_address,
+      building_address: Faker::Address.building_number,
+      zip_code: Faker::Address.zip_code
+  )
+
+    shop = Shop.create(
+      address:      address,
+      title:        Faker::Restaurant.name,
+      description:  Faker::Restaurant.description,
+      logo:         Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
+      state:        0,
+      is_active:    true
+    )
+
+    user = User.new(
+      first_name:   Faker::Name.first_name ,
+      last_name:    Faker::Name.last_name,
+      email:        Faker::Internet.email,
+      password:     Faker::Internet.password(min_length: 6, max_length: 20),
+      avator:       Faker::Avatar.image(slug: "my-own-slug", size: "50x50"),
+      phone:        Faker::PhoneNumber.cell_phone
+    )
+
+    ShopOwner.create(
+      user: user,
+      shop: shop,
+      apply_state:0
+    )
 end

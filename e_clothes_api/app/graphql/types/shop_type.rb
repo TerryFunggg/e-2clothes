fa05a9d2fbe6436a25f73c5ccf7a4a9cf80e0603
TerryFunggg <terryyessfung@gmail.com>
2021-03-01 16:40:49 +0800

    modify shop assocation

--- e_clothes_api/app/graphql/types/shop_type.rb
+++ e_clothes_api/app/graphql/types/shop_type.rb
@@ -2,6 +2,7 @@ module Types
   class ShopType < Types::BaseObject
     field :id, ID, null: false
     field :address, Types::AddressType, null: true
+    field :users, [Types::UserType], null: true
     field :logo, String, null: true
     field :title, String, null: false
     field :description, String, null: true
--- e_clothes_api/app/models/shop.rb
+++ e_clothes_api/app/models/shop.rb
@@ -1,6 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :address
-  has_many   :shop_owners
-  has_one    :users, through: :shop_owners
+  has_many    :shop_owners
+  has_many    :users, through: :shop_owners
   enum state: { opening: 0, close: 1 }
 end

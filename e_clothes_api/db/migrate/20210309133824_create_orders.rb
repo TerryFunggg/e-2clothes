582ca45d84e8ae3dad77d17167fd3de85b21c0a9
TerryFunggg <terryyessfung@gmail.com>
2021-03-09 21:41:50 +0800

    order model created

---
+++ e_clothes_api/db/migrate/20210309133824_create_orders.rb
@@ -0,0 +1,15 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :address
+      t.string :city
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
--- e_clothes_api/db/schema.rb
+++ e_clothes_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_072847) do
+ActiveRecord::Schema.define(version: 2021_03_09_133824) do
 
   create_table "addresses", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id", null: false
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 2021_02_28_072847) do
 
   create_table "cities", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", charset: "utf8mb4", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.string "city"
+    t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

4e121e871935e7e5480483e9f6c84e9a1b5111af
TerryFunggg <terryyessfung@gmail.com>
2021-03-13 13:22:08 +0800

    update user mutatuion

---
+++ e_clothes_api/app/graphql/mutations/users/create_user.rb
@@ -0,0 +1,29 @@
+module Mutations
+  module Users
+    class CreateUser < BaseMutation
+      argument :user, Types::Users::UserAttributes, required: true
+      argument :address, Types::AddressAttributes, required: false
+
+      field :user,    Types::Users::UserType, null: true
+      field :errors,  Types::ValidationErrorsType, null: true
+
+      def resolve(user:, address: {})
+        user = User.new(user.to_h)
+        user.address = create_address(address.to_h) unless address.empty?
+
+        if user.save
+          { user: user }
+        else
+          { errors: user.errors }
+        end
+      end
+
+      def create_address(address)
+        # TODO Error handling
+        address = CreateAddress.create(address)
+        address.save
+        address
+      end
+    end
+  end
+end

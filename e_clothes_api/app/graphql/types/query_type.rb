f12bda9fdac496eaa2c5b4833468f9fa0b1fa511
TerryFunggg <terryyessfung@gmail.com>
2021-03-15 12:41:59 +0800

    update querytype file

--- e_clothes_api/app/graphql/types/query_type.rb
+++ e_clothes_api/app/graphql/types/query_type.rb
@@ -4,9 +4,23 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
+    # TODO: Refector
+
     # /address
     field :addresses, [Types::Address::AddressType], description: "Return a list of address", null: false
     field :address, Types::Address::AddressType, description: "Get address by ID", null: false do
+      argument :id, ID, required: true
+    end
+
+    # /user
+    field :users, [Types::Users::UserType], description: "Return a list of User", null: false
+    field :user, Types::Users::UserType, description: "Get User by ID", null: false do
+      argument :id, ID, required: true
+    end
+
+    # /shop
+    field :shops, [Types::ShopType], null: false, description: "Return a list of shops"
+    field :shop,  Types::ShopType, null: false, description: "Return a shop object" do
       argument :id, ID, required: true
     end
 
@@ -18,11 +32,6 @@ module Types
       Address.find(id)
     end
 
-    # /user
-    field :users, [Types::Users::UserType], description: "Return a list of User", null: false
-    field :user, Types::Users::UserType, description: "Get User by ID", null: false do
-      argument :id, ID, required: true
-    end
 
     def users
       User.all
@@ -32,11 +41,6 @@ module Types
       User.find(id)
     end
 
-    # /shop
-    field :shops, [Types::ShopType], null: false, description: "Return a list of shops"
-    field :shop,  Types::ShopType, null: false, description: "Return a shop object" do
-      argument :id, ID, required: true
-    end
 
     def shop(id:)
       # Shop.select("shops.*,users.*").joins(:users).find_by('shop_owners.user_id = ?', 21)
